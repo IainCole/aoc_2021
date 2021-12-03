c24ecc0de60358f704806af71f93bd60b8e960f6
Cole <coleiain@amazon.com>
2021-12-03 15:18:54 -0800

    fix random shit

--- 01/solution-pt2.sql
+++ 01/solution-pt2.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS input;
 
 CREATE TEMP TABLE input(depth text);
 
-COPY input FROM 'enecccbccvtdfbnnjlihfiufbkghujkbdedtltgflrdj' ||
-    '/aoc_2021/01/input';
+COPY input FROM '/Users/coleiain/aoc_2021/01/input';
 
 WITH indexed as (
     SELECT ROW_NUMBER() OVER(ORDER BY 1) as row_num, CAST(depth as int)
--- 02/solution-pt2.sql
+++ 02/solution-pt2.sql
@@ -1,148 +1,21 @@
-DROP TYPE IF EXISTS deckrow;
-CREATE TYPE deckrow AS (f1 int, f2 int);
+DROP TABLE IF EXISTS input;
 
-WITH RECURSIVE deck as (
-    VALUES
-        (gen_random_uuid(), 1),
-        (gen_random_uuid(), 2),
-        (gen_random_uuid(), 3),
-        (gen_random_uuid(), 4),
-        (gen_random_uuid(), 5),
-        (gen_random_uuid(), 6),
-        (gen_random_uuid(), 7),
-        (gen_random_uuid(), 8),
-        (gen_random_uuid(), 9),
-        (gen_random_uuid(), 10)
-), ordered_deck as (
-    SELECT ROW_NUMBER() OVER (ORDER BY column1) as idx, column2 as card_value
-    FROM deck
-    ORDER BY column1
-), p1 as (
-    SELECT ROW_NUMBER() OVER (ORDER BY idx) as idx, card_value
-    FROM ordered_deck
-    WHERE idx <= 5
-    ORDER BY idx
-), p2 as (
-    SELECT ROW_NUMBER() OVER (ORDER BY idx) as idx, card_value
-    FROM ordered_deck
-    WHERE idx > 5
-    ORDER BY idx
-), round(p1h, p2h, round_num) AS (
-    SELECT (SELECT json_agg(row_to_json((idx, card_value))) FROM p1), (SELECT json_agg(row_to_json((idx, card_value))) FROM p2), 1
-    UNION ALL
-    SELECT
-        (
-            SELECT json_agg(row_to_json((f1, f2)))
-            FROM (
-                     SELECT *
-                     FROM json_populate_recordset(null::deckrow, p1h) p1t
-                     WHERE p1t.f1 > round_num
-                     UNION ALL
-                     SELECT * FROM (
-                                       VALUES
-                                           (
-                                               (
-                                                   SELECT MAX(f1) + 1
-                                                   FROM json_populate_recordset(null::deckrow, p1h)
-                                               ),
-                                               (
-                                                   SELECT f2
-                                                   FROM json_populate_recordset(null::deckrow, p1h)
-                                                   WHERE f1 = round_num
-                                               )
-                                           ),
-                                           (
-                                               (
-                                                   SELECT MAX(f1) + 2
-                                                   FROM json_populate_recordset(null::deckrow, p1h)
-                                               ),
-                                               (
-                                                   SELECT f2
-                                                   FROM json_populate_recordset(null::deckrow, p2h)
-                                                   WHERE f1 = round_num
-                                               )
-                                           )
-                                   ) as wincheck
-                     WHERE
-                             (
-                                 SELECT f2
-                                 FROM json_populate_recordset(null::deckrow, p1h)
-                                 WHERE f1 = round_num
-                             )
-                             >
-                             (
-                                 SELECT f2
-                                 FROM json_populate_recordset(null::deckrow, p2h)
-                                 WHERE f1 = round_num
-                             )
-                 ) as p1r
-        ),
-        (
-            SELECT json_agg(row_to_json((f1, f2)))
-            FROM (
-                     SELECT *
-                     FROM json_populate_recordset(null::deckrow, p2h) p2t
-                     WHERE p2t.f1 > round_num
-                     UNION ALL
-                     SELECT * FROM (
-                                       VALUES
-                                           (
-                                               (
-                                                   SELECT MAX(f1) + 1
-                                                   FROM json_populate_recordset(null::deckrow, p2h)
-                                               ),
-                                               (
-                                                   SELECT f2
-                                                   FROM json_populate_recordset(null::deckrow, p2h)
-                                                   WHERE f1 = round_num
-                                               )
-                                           ),
-                                           (
-                                               (
-                                                   SELECT MAX(f1) + 2
-                                                   FROM json_populate_recordset(null::deckrow, p2h)
-                                               ),
-                                               (
-                                                   SELECT f2
-                                                   FROM json_populate_recordset(null::deckrow, p1h)
-                                                   WHERE f1 = round_num
-                                               )
-                                           )
-                                   ) as wincheck
-                     WHERE
-                             (
-                                 SELECT f2
-                                 FROM json_populate_recordset(null::deckrow, p2h)
-                                 WHERE f1 = round_num
-                             )
-                             >
-                             (
-                                 SELECT f2
-                                 FROM json_populate_recordset(null::deckrow, p1h)
-                                 WHERE f1 = round_num
-                             )
-                 ) as p2r
-        ),
-        round_num + 1
-    FROM round
-    WHERE LEAST(
-                  (SELECT COUNT(*) as p1c FROM json_populate_recordset(null::deckrow, p1h) p1t WHERE p1t.f1 >= round_num),
-                  (SELECT COUNT(*) as p2c FROM json_populate_recordset(null::deckrow, p2h) p2t WHERE p2t.f1 >= round_num)
-              ) != 0
-      AND
-            round_num <= 100
+CREATE TEMP TABLE input(raw text);
+
+COPY input FROM '/Users/coleiain/aoc_2021/02/input';
+
+with instructions as (
+    SELECT ROW_NUMBER() OVER (ORDER BY 1) as row_num, split_part(raw, ' ', 1) as instruction, CAST(split_part(raw, ' ', 2) as int) as amount
+    FROM input
+), with_aim as (
+    SELECT i.row_num, i.instruction, i.amount, SUM(CASE WHEN i2.instruction = 'down' THEN i2.amount WHEN i2.instruction = 'up' THEN i2.amount * -1 ELSE 0 END) as aim
+    FROM instructions i
+    LEFT JOIN instructions i2 on i2.row_num < i.row_num
+    GROUP BY i.row_num, i.instruction, i.amount
+), components as (
+    SELECT SUM(CASE WHEN instruction = 'forward' THEN amount ELSE 0 END)       as horizontal,
+           SUM(CASE WHEN instruction = 'forward' THEN amount * aim ELSE 0 END) as vertical
+    FROM with_aim
 )
-SELECT
-    (SELECT json_agg(to_json((f2))) FROM json_populate_recordset(null::deckrow, p1h)) as p1_hand,
-    (SELECT json_agg(to_json((f2))) FROM json_populate_recordset(null::deckrow, p2h)) as p2_hand,
-    CASE WHEN
-                 (SELECT COUNT(*) as p1c FROM json_populate_recordset(null::deckrow, p1h)) = 10
-             THEN
-             CONCAT(CAST('Player 1 wins with a score of ' as text), CAST((SELECT SUM(f2 * (10 - (p1sum.f1 - rds.round_num))) FROM json_populate_recordset(null::deckrow, p1h) p1sum  WHERE p1sum.f1 >= rds.round_num) as text))
-         WHEN (SELECT COUNT(*) as p2c FROM json_populate_recordset(null::deckrow, p2h)) = 10
-             THEN
-             CONCAT(CAST('Player 2 wins with a score of ' as text), CAST((SELECT SUM(f2 * (10 - (p2sum.f1 - rds.round_num))) FROM json_populate_recordset(null::deckrow, p2h) p2sum  WHERE p2sum.f1 >= rds.round_num) as text))
-         ELSE
-             'Player 1 wins after stalemate'
-        END as score
-FROM round rds ORDER BY round_num DESC LIMIT 1;
+SELECT horizontal, vertical, horizontal * vertical as solution
+FROM components;
